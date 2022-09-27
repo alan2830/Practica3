7967eb41ff0972a001e4b2f774094824ffcbe7da
Gama´s Product <Gamas Product@DESKTOP-S14JKI4>
2022-09-26 18:20:08 -0700

    Agregar archivos de proyecto.

---
+++ Practica3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numlados;
+            float medidalado, perimetro;
+
+            Console.WriteLine("escriba el numero de los lados: ");
+            numlados = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("escriba la medida de un lado: "); 
+            medidalado = Single.Parse(Console.ReadLine());
+            perimetro = numlados * medidalado; 
+
+            Console.WriteLine("el perimetro es: {0}", perimetro);
+
+                
+                
+        }
+    }
+}
